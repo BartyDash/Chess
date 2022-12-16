b59c155b70abccf4b5b28d55eec87e5f614a1d8a
BartyDash <writetobartek@gmail.com>
2022-12-16 01:21:48 +0100

    printing board with pawns in number format from array

--- test-menuPlay.c
+++ test-menuPlay.c
@@ -6,13 +6,23 @@
 #include <stdbool.h>
 
 void SetCMDSizeAndTitle(short width, short height, char *title);
-void printLine(int iSize, int color1, int color2);
+void printLine(int iSize, int color1, int color2, int row);
 void printBoard(int iSize);
 
 #define white 0xDB
 #define black 0xFF
 
 int iBoardSize = 8;
+int board[8][8] = {
+        2,3,4,6,5,4,3,2,
+        1,1,1,1,1,1,1,1,
+        0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,
+        0,0,0,0,0,0,0,0,
+        1,1,1,1,1,1,1,1,
+        2,3,4,6,5,4,3,2
+};
 
 int main(void){
     SetCMDSizeAndTitle(120, 40, "Szachy");
@@ -22,14 +32,18 @@ int main(void){
     getch();
 }
 
-void printLine(int iSize, int color1, int color2){
+void printLine(int iSize, int color1, int color2, int row){
 
     int cell = 6;
 
     for (int subLine = 0; subLine < cell / 2; ++subLine) {
         for (int i = 0; i < iSize; ++i) {
+            int column = i;
             for (int j = 0; j < cell; ++j) {
-                if(i%2 == 0){
+                if (subLine == cell / 2 / 2 && j == cell / 2 && board[row][column]){
+                    printf("%d", board[row][column]);
+                }
+                else if(i%2 == 0){
                     printf("%c", color1);
                 } else{
                     printf("%c", color2);
@@ -44,10 +58,11 @@ void printLine(int iSize, int color1, int color2){
 
 void printBoard(int iSize){
     for (int i = 0; i < iSize; ++i) {
+        int row = i;
         if(i%2 == 0){
-            printLine(iSize, white, black);
+            printLine(iSize, white, black, row);
         } else{
-            printLine(iSize, black, white);
+            printLine(iSize, black, white, row);
         }
     }
 }
